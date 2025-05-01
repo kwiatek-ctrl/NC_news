53779e0a0121217b43cecd505600f374b65c425a
Antek Nguyen Manh <ak@Mac.communityfibre.co.uk>
2025-05-01 12:43:55 +0100

    small changes

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const ENV = process.env.NODE_ENV || 'development'
 
 require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
 
-const db = new Pool(config);
+const db = new Pool();
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
     throw new Error("No PGDATABASE or DATABASE_URL configured")
@@ -19,4 +19,4 @@ if (ENV === "production") {
     config.max = 2;
 }
 
-module.exports = db;
+module.exports = new Pool(config);
